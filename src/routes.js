39c21856ef9784bcc72fa341a65c1b2a02dc7dce
rongqui <rongmaphunkhoi@gmail.com>
2022-02-24 21:23:52 +0700

    api company table

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ const PlanDetail = React.lazy(() => import('./views/plan/PlanDetail'))
 const CompanyTree = React.lazy(() => import('./views/pages/companytree/CompanyTree'))
 const KpiRegistration = React.lazy(() => import('./views/kpiregistration/KpiRegistration'))
 const EditKpiAndWeightView = React.lazy(() => import('./views/plan/EditKpiAndWeightView'))
+const CompanyTable = React.lazy(() => import('./views/pages/companytree/CompanyTable'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
@@ -26,7 +27,8 @@ const routes = [
   { path: '/users', name: 'User', component: UserPage },
   { path: '/depts', exact: true, name: 'Department', component: Department },
   { path: '/depts/:id', name: 'User In Department', component: UserDepartment },
-  { path: '/companytree', name: 'Company', component: CompanyTree },
+  { path: '/companytree', exact: true, name: 'Company', component: CompanyTree },
+  { path: '/companytree/:id', exact: true, name: 'Company Table', component: CompanyTable },
   { path: '/kpiregistration', name: 'KPI Registration', component: KpiRegistration },
 ]
 
