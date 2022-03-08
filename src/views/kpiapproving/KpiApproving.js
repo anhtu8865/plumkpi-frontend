62378355d747c3bf274c9c922862f0a3e20ecc27
rongqui <rongmaphunkhoi@gmail.com>
2022-03-08 09:39:03 +0700

    duyệt kpi

--- src/views/kpiapproving/KpiApproving.js
+++ src/views/kpiapproving/KpiApproving.js
@@ -14,9 +14,16 @@ import {
   CTableHead,
   CTableHeaderCell,
   CTableRow,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
 } from '@coreui/react'
 import FilterAltIcon from '@mui/icons-material/FilterAlt'
 import SearchIcon from '@mui/icons-material/Search'
+import CheckIcon from '@mui/icons-material/Check'
+import DoDisturbIcon from '@mui/icons-material/DoDisturb'
 import { Avatar, Button, Grid, Pagination, Checkbox } from '@mui/material'
 import { Field, FormikProvider, useFormik } from 'formik'
 import PropTypes from 'prop-types'
@@ -25,12 +32,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { LoadingCircle } from 'src/components/LoadingCircle'
 import SystemAlert from 'src/components/SystemAlert'
 import { createAlert } from 'src/slices/alertSlice'
-import { setKpiApprovingLoading } from 'src/slices/kpiApprovingSlice'
+import { setKpiApprovingLoading, setKpiApprovingReload } from 'src/slices/kpiApprovingSlice'
 import api from 'src/views/axiosConfig'
 import { useParams } from 'react-router-dom'
 
 import { InfoKpiApproving } from './InfoKpiApproving'
-import AcceptedKpi from './AcceptedKpiApproving'
+//import AcceptedKpi from './AcceptedKpiApproving'
 
 function colorStatus(status) {
   if (status == 'Đang xử lý') return 'text-warning'
@@ -88,6 +95,230 @@ const KpiApproving = () => {
 
     fetchKPIList()
   }, [kpiApprovingReload, dispatch])
+
+  const AcceptedKpiApproving = (props) => {
+    const dispatch = useDispatch()
+    const { kpiApprovingReload, kpiApprovingLoading } = useSelector((state) => state.kpiApproving)
+
+    const [modalVisible, setModalVisible] = React.useState(false)
+    const [isSubmit, setIsSubmit] = React.useState(false)
+
+    const [acceptList, setAcceptList] = React.useState([])
+
+    React.useEffect(() => {
+      async function createRowsAccept() {
+        //console.log(props.selectedKpi.length)
+        let newAccList = []
+        for (let i = 0; i < props.selectedKpi.length; i++) {
+          let tmp = {
+            plan: {
+              plan_id: 3,
+            },
+            kpi_template: {
+              kpi_template_id: 5,
+            },
+            approve_registration: 'Chấp nhận',
+          }
+          tmp.plan.plan_id = props.selectedKpi[i].plan.plan_id
+          tmp.kpi_template.kpi_template_id = props.selectedKpi[i].kpi_template.kpi_template_id
+          newAccList.push(tmp)
+        }
+
+        setAcceptList(newAccList)
+      }
+      createRowsAccept()
+    }, [props.selectedKpi, kpiApprovingReload])
+
+    const onClickAccept = () => {
+      setIsSubmit(true)
+
+      api
+        .put(`/plans/plan/${props.plan_id}/personal-kpis`, { rows: acceptList })
+        .then(() => {
+          //console.log(acceptList)
+          dispatch(
+            createAlert({
+              message: 'Chấp nhận KPI thành công.',
+              type: 'success',
+            }),
+          )
+          dispatch(
+            setKpiApprovingLoading({
+              value: true,
+            }),
+          )
+          dispatch(setKpiApprovingReload())
+          setModalVisible(false)
+        })
+        .catch((error) => {
+          dispatch(
+            createAlert({
+              message: error.response.data.message,
+              type: 'error',
+            }),
+          )
+        })
+        .finally(() => {
+          setIsSubmit(false)
+        })
+    }
+
+    return (
+      <>
+        <Button
+          variant="contained"
+          color="success"
+          startIcon={<CheckIcon />}
+          onClick={() => setModalVisible(true)}
+        >
+          Chấp nhận
+        </Button>
+        <CModal
+          alignment="center"
+          scrollable
+          visible={modalVisible}
+          onClose={() => {
+            setModalVisible(false)
+          }}
+        >
+          <CModalHeader>
+            <CModalTitle>Duyệt KPI</CModalTitle>
+          </CModalHeader>
+          <CModalBody className="mx-4 mb-3">
+            Bạn có chắc chắn muốn chấp nhận KPI đã chọn ?
+          </CModalBody>
+          <CModalFooter>
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<CheckIcon />}
+              type="submit"
+              onClick={() => onClickAccept()}
+              disabled={isSubmit}
+            >
+              Xác nhận
+            </Button>
+            {isSubmit && <LoadingCircle />}
+          </CModalFooter>
+        </CModal>
+      </>
+    )
+  }
+  AcceptedKpiApproving.propTypes = {
+    selectedKpi: PropTypes.array,
+    plan_id: PropTypes.string,
+  }
+
+  //Denied
+  const DeniedKpiApproving = (props) => {
+    const dispatch = useDispatch()
+    const { kpiApprovingReload, kpiApprovingLoading } = useSelector((state) => state.kpiApproving)
+
+    const [modalVisible, setModalVisible] = React.useState(false)
+    const [isSubmit, setIsSubmit] = React.useState(false)
+
+    const [acceptList, setAcceptList] = React.useState([])
+
+    React.useEffect(() => {
+      async function createRowsAccept() {
+        let newAccList = []
+        for (let i = 0; i < props.selectedKpi.length; i++) {
+          let tmp = {
+            plan: {
+              plan_id: 3,
+            },
+            kpi_template: {
+              kpi_template_id: 5,
+            },
+            approve_registration: 'Từ chối',
+          }
+          tmp.plan.plan_id = props.selectedKpi[i].plan.plan_id
+          tmp.kpi_template.kpi_template_id = props.selectedKpi[i].kpi_template.kpi_template_id
+          newAccList.push(tmp)
+        }
+
+        setAcceptList(newAccList)
+      }
+      createRowsAccept()
+    }, [props.selectedKpi, kpiApprovingReload])
+
+    const onClickAccept = () => {
+      setIsSubmit(true)
+
+      api
+        .put(`/plans/plan/${props.plan_id}/personal-kpis`, { rows: acceptList })
+        .then(() => {
+          //console.log(acceptList)
+          dispatch(
+            createAlert({
+              message: 'Chấp nhận KPI thành công.',
+              type: 'success',
+            }),
+          )
+          dispatch(
+            setKpiApprovingLoading({
+              value: true,
+            }),
+          )
+          dispatch(setKpiApprovingReload())
+          setModalVisible(false)
+        })
+        .catch((error) => {
+          dispatch(
+            createAlert({
+              message: error.response.data.message,
+              type: 'error',
+            }),
+          )
+        })
+        .finally(() => {
+          setIsSubmit(false)
+        })
+    }
+
+    return (
+      <>
+        <Button
+          variant="contained"
+          color="error"
+          startIcon={<DoDisturbIcon />}
+          onClick={() => setModalVisible(true)}
+        >
+          Từ chối
+        </Button>
+        <CModal
+          alignment="center"
+          scrollable
+          visible={modalVisible}
+          onClose={() => {
+            setModalVisible(false)
+          }}
+        >
+          <CModalHeader>
+            <CModalTitle>Duyệt KPI</CModalTitle>
+          </CModalHeader>
+          <CModalBody className="mx-4 mb-3">Bạn có chắc chắn muốn từ chối KPI đã chọn ?</CModalBody>
+          <CModalFooter>
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<CheckIcon />}
+              type="submit"
+              onClick={() => onClickAccept()}
+              disabled={isSubmit}
+            >
+              Xác nhận
+            </Button>
+            {isSubmit && <LoadingCircle />}
+          </CModalFooter>
+        </CModal>
+      </>
+    )
+  }
+  DeniedKpiApproving.propTypes = {
+    selectedKpi: PropTypes.array,
+    plan_id: PropTypes.string,
+  }
 
   const KpiApprovingTable = (props) => {
     return (
@@ -173,7 +404,8 @@ const KpiApproving = () => {
                   </CCol>
                   <CCol xs={6}>
                     <div className="d-grid gap-3 d-md-flex justify-content-end">
-                      <AcceptedKpi plan_id={id} selectedKpi={selectedKpi} />
+                      <AcceptedKpiApproving plan_id={id} selectedKpi={selectedKpi} />
+                      <DeniedKpiApproving plan_id={id} selectedKpi={selectedKpi} />
                     </div>
                   </CCol>
                 </CRow>
